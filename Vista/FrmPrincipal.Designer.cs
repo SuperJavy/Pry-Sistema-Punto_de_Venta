cd46fcc94ee54cb4a94d7210c074d3ce977ce10b
Angel Mauricio <mauromh951@gmail.com>
2026-05-29 00:16:29 -0600

    Modificaciones de interfaces y agrupacion de carpetas

---
+++ Vista/FrmPrincipal.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace Pry_Sistema_Punto_de_Venta
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmPrincipal));
+            pnlcontenedor = new Panel();
+            panel1 = new Panel();
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            menuStrip1 = new MenuStrip();
+            ventaToolStripMenuItem = new ToolStripMenuItem();
+            ventasToolStripMenuItem = new ToolStripMenuItem();
+            agregarProductoToolStripMenuItem = new ToolStripMenuItem();
+            compraToolStripMenuItem = new ToolStripMenuItem();
+            inventarioToolStripMenuItem = new ToolStripMenuItem();
+            agregraToolStripMenuItem = new ToolStripMenuItem();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pnlcontenedor
+            // 
+            pnlcontenedor.BackColor = Color.White;
+            pnlcontenedor.Dock = DockStyle.Fill;
+            pnlcontenedor.Location = new Point(0, 98);
+            pnlcontenedor.Margin = new Padding(3, 2, 3, 2);
+            pnlcontenedor.Name = "pnlcontenedor";
+            pnlcontenedor.Size = new Size(904, 288);
+            pnlcontenedor.TabIndex = 1;
+            pnlcontenedor.Paint += pnlcontenedor_Paint;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ActiveCaption;
+            panel1.Controls.Add(pictureBox1);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(menuStrip1);
+            panel1.Dock = DockStyle.Top;
+            panel1.ForeColor = Color.AliceBlue;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(904, 98);
+            panel1.TabIndex = 2;
+            panel1.Paint += panel1_Paint;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(12, 3);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(78, 55);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 4;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = SystemColors.ActiveCaption;
+            label1.Font = new Font("Microsoft Sans Serif", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Black;
+            label1.Location = new Point(107, 18);
+            label1.Name = "label1";
+            label1.Size = new Size(480, 40);
+            label1.TabIndex = 3;
+            label1.Text = "Sistema de Punto de Venta";
+            label1.Click += label1_Click;
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Dock = DockStyle.Bottom;
+            menuStrip1.Font = new Font("Microsoft YaHei", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { ventaToolStripMenuItem, ventasToolStripMenuItem, agregarProductoToolStripMenuItem, compraToolStripMenuItem, inventarioToolStripMenuItem, agregraToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 59);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Padding = new Padding(4, 2, 0, 2);
+            menuStrip1.Size = new Size(904, 39);
+            menuStrip1.TabIndex = 2;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // ventaToolStripMenuItem
+            // 
+            ventaToolStripMenuItem.Name = "ventaToolStripMenuItem";
+            ventaToolStripMenuItem.Size = new Size(16, 35);
+            // 
+            // ventasToolStripMenuItem
+            // 
+            ventasToolStripMenuItem.Image = (Image)resources.GetObject("ventasToolStripMenuItem.Image");
+            ventasToolStripMenuItem.Name = "ventasToolStripMenuItem";
+            ventasToolStripMenuItem.Size = new Size(128, 35);
+            ventasToolStripMenuItem.Text = "Ventas";
+            ventasToolStripMenuItem.Click += ventasToolStripMenuItem_Click;
+            // 
+            // agregarProductoToolStripMenuItem
+            // 
+            agregarProductoToolStripMenuItem.Image = (Image)resources.GetObject("agregarProductoToolStripMenuItem.Image");
+            agregarProductoToolStripMenuItem.Name = "agregarProductoToolStripMenuItem";
+            agregarProductoToolStripMenuItem.Size = new Size(166, 35);
+            agregarProductoToolStripMenuItem.Text = "Productos";
+            agregarProductoToolStripMenuItem.Click += agregarProductoToolStripMenuItem_Click;
+            // 
+            // compraToolStripMenuItem
+            // 
+            compraToolStripMenuItem.Font = new Font("Microsoft YaHei", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            compraToolStripMenuItem.Image = (Image)resources.GetObject("compraToolStripMenuItem.Image");
+            compraToolStripMenuItem.Name = "compraToolStripMenuItem";
+            compraToolStripMenuItem.Size = new Size(151, 35);
+            compraToolStripMenuItem.Text = "Compras";
+            compraToolStripMenuItem.Click += compraToolStripMenuItem_Click;
+            // 
+            // inventarioToolStripMenuItem
+            // 
+            inventarioToolStripMenuItem.Image = (Image)resources.GetObject("inventarioToolStripMenuItem.Image");
+            inventarioToolStripMenuItem.Name = "inventarioToolStripMenuItem";
+            inventarioToolStripMenuItem.Size = new Size(166, 35);
+            inventarioToolStripMenuItem.Text = "Inventario";
+            inventarioToolStripMenuItem.Click += inventarioToolStripMenuItem_Click;
+            // 
+            // agregraToolStripMenuItem
+            // 
+            agregraToolStripMenuItem.Image = Properties.Resources.configuraciones;
+            agregraToolStripMenuItem.Name = "agregraToolStripMenuItem";
+            agregraToolStripMenuItem.Size = new Size(235, 35);
+            agregraToolStripMenuItem.Text = "Configuraciones";
+            agregraToolStripMenuItem.Click += agregraToolStripMenuItem_Click;
+            // 
+            // FrmPrincipal
+            // 
+            AutoScaleDimensions = new SizeF(10F, 22F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveBorder;
+            ClientSize = new Size(904, 386);
+            Controls.Add(pnlcontenedor);
+            Controls.Add(panel1);
+            Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            ForeColor = SystemColors.ActiveCaptionText;
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Margin = new Padding(3, 2, 3, 2);
+            Name = "FrmPrincipal";
+            RightToLeft = RightToLeft.No;
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Sistema de ⁯Punto de Venta";
+            WindowState = FormWindowState.Maximized;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel pnlcontenedor;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ventaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem inventarioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem agregarProductoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem compraToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem agregraToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStripMenuItem ventasToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
