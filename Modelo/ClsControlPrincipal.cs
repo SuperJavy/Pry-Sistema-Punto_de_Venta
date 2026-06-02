b305237db5c3f43c2908dda04869031dc7855ebd
johanjavier <20241054@uthh.edu.mx>
2026-06-02 13:12:08 -0600

    Refactorización del proyecto y nueva clase Cls
    Se eliminaron las carpetas `Modelo\` y `Controlador\` del archivo
    de proyecto `Pry-Sistema_Punto_de_Venta.csproj`.
    
    Se añadió la clase `Cls` en el espacio de nombres
    `Pry_Sistema_Punto_de_Venta.Modelo` dentro del archivo
    `ClsControlPrincipal.cs`.
    
    Se agregaron referencias a los espacios de nombres necesarios
    en `ClsControlPrincipal.cs` (`System`, `System.Collections.Generic`,
    `System.Linq`, `System.Text`, `System.Threading.Tasks`).

---
+++ Modelo/ClsControlPrincipal.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pry_Sistema_Punto_de_Venta.Modelo
+{
+    internal class Cls
+    {
+    }
+}
