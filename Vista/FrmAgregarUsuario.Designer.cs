f508816d3ba81a997453ddb6dba33eee2bfb99e5
johanjavier <20241054@uthh.edu.mx>
2026-06-04 13:36:57 -0600

    Ajuste de diseño y nuevo Label para selección de rol
    Se realizaron los siguientes cambios en el formulario `FrmAgregarUsuario`:
    - Se agregó un nuevo control `Label` para indicar la selección de rol.
    - Se ajustaron las posiciones y tamaños de los controles para un diseño más compacto.
    - Se redujeron los márgenes y dimensiones generales del formulario.
    - Se optimizó el tamaño de los controles de entrada de texto y botones.
    - Se ajustaron las dimensiones de escala automática para mejorar la visualización.

--- Vista/FrmAgregarUsuario.Designer.cs
+++ Vista/FrmAgregarUsuario.Designer.cs
@@ -43,6 +43,7 @@
             txtPassword = new TextBox();
             BtnAgregarUsuario = new Button();
             cmbRol = new ComboBox();
+            label1 = new Label();
             SuspendLayout();
             // 
             // lblTitulo
@@ -50,10 +51,10 @@
             lblTitulo.AutoSize = true;
             lblTitulo.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
             lblTitulo.ForeColor = Color.FromArgb(44, 62, 80);
-            lblTitulo.Location = new Point(48, 36);
-            lblTitulo.Margin = new Padding(5, 0, 5, 0);
+            lblTitulo.Location = new Point(40, 30);
+            lblTitulo.Margin = new Padding(4, 0, 4, 0);
             lblTitulo.Name = "lblTitulo";
-            lblTitulo.Size = new Size(399, 45);
+            lblTitulo.Size = new Size(336, 38);
             lblTitulo.TabIndex = 0;
             lblTitulo.Text = "Registrar Nuevo Usuario";
             // 
@@ -62,10 +63,10 @@
             lblNombre.AutoSize = true;
             lblNombre.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold);
             lblNombre.ForeColor = Color.FromArgb(127, 140, 141);
-            lblNombre.Location = new Point(53, 126);
-            lblNombre.Margin = new Padding(5, 0, 5, 0);
+            lblNombre.Location = new Point(44, 105);
+            lblNombre.Margin = new Padding(4, 0, 4, 0);
             lblNombre.Name = "lblNombre";
-            lblNombre.Size = new Size(116, 32);
+            lblNombre.Size = new Size(98, 28);
             lblNombre.TabIndex = 1;
             lblNombre.Text = "Nombre :";
             // 
@@ -74,10 +75,10 @@
             lblApellidoPaterno.AutoSize = true;
             lblApellidoPaterno.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold);
             lblApellidoPaterno.ForeColor = Color.FromArgb(127, 140, 141);
-            lblApellidoPaterno.Location = new Point(53, 228);
-            lblApellidoPaterno.Margin = new Padding(5, 0, 5, 0);
+            lblApellidoPaterno.Location = new Point(44, 190);
+            lblApellidoPaterno.Margin = new Padding(4, 0, 4, 0);
             lblApellidoPaterno.Name = "lblApellidoPaterno";
-            lblApellidoPaterno.Size = new Size(208, 32);
+            lblApellidoPaterno.Size = new Size(174, 28);
             lblApellidoPaterno.TabIndex = 3;
             lblApellidoPaterno.Text = "Apellido Paterno :";
             // 
@@ -86,10 +87,10 @@
             lblApellidoMaterno.AutoSize = true;
             lblApellidoMaterno.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold);
             lblApellidoMaterno.ForeColor = Color.FromArgb(127, 140, 141);
-            lblApellidoMaterno.Location = new Point(53, 330);
-            lblApellidoMaterno.Margin = new Padding(5, 0, 5, 0);
+            lblApellidoMaterno.Location = new Point(44, 275);
+            lblApellidoMaterno.Margin = new Padding(4, 0, 4, 0);
             lblApellidoMaterno.Name = "lblApellidoMaterno";
-            lblApellidoMaterno.Size = new Size(217, 32);
+            lblApellidoMaterno.Size = new Size(181, 28);
             lblApellidoMaterno.TabIndex = 5;
             lblApellidoMaterno.Text = "Apellido Materno :";
             // 
@@ -98,10 +99,10 @@
             lblDireccion.AutoSize = true;
             lblDireccion.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold);
             lblDireccion.ForeColor = Color.FromArgb(127, 140, 141);
-            lblDireccion.Location = new Point(528, 126);
-            lblDireccion.Margin = new Padding(5, 0, 5, 0);
+            lblDireccion.Location = new Point(440, 105);
+            lblDireccion.Margin = new Padding(4, 0, 4, 0);
             lblDireccion.Name = "lblDireccion";
-            lblDireccion.Size = new Size(128, 32);
+            lblDireccion.Size = new Size(107, 28);
             lblDireccion.TabIndex = 7;
             lblDireccion.Text = "Dirección :";
             // 
@@ -110,10 +111,10 @@
             lblTelefono.AutoSize = true;
             lblTelefono.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold);
             lblTelefono.ForeColor = Color.FromArgb(127, 140, 141);
-            lblTelefono.Location = new Point(528, 228);
-            lblTelefono.Margin = new Padding(5, 0, 5, 0);
+            lblTelefono.Location = new Point(440, 190);
+            lblTelefono.Margin = new Padding(4, 0, 4, 0);
             lblTelefono.Name = "lblTelefono";
-            lblTelefono.Size = new Size(120, 32);
+            lblTelefono.Size = new Size(102, 28);
             lblTelefono.TabIndex = 9;
             lblTelefono.Text = "Teléfono :";
             // 
@@ -122,65 +123,65 @@
             lblContrasena.AutoSize = true;
             lblContrasena.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold);
             lblContrasena.ForeColor = Color.FromArgb(127, 140, 141);
-            lblContrasena.Location = new Point(528, 330);
-            lblContrasena.Margin = new Padding(5, 0, 5, 0);
+            lblContrasena.Location = new Point(440, 275);
+            lblContrasena.Margin = new Padding(4, 0, 4, 0);
             lblContrasena.Name = "lblContrasena";
-            lblContrasena.Size = new Size(151, 32);
+            lblContrasena.Size = new Size(125, 28);
             lblContrasena.TabIndex = 11;
             lblContrasena.Text = "Contraseña :";
             // 
             // txtNombre
             // 
             txtNombre.Font = new Font("Segoe UI", 11F);
-            txtNombre.Location = new Point(53, 160);
-            txtNombre.Margin = new Padding(5, 7, 5, 7);
+            txtNombre.Location = new Point(44, 133);
+            txtNombre.Margin = new Padding(4, 6, 4, 6);
             txtNombre.Name = "txtNombre";
-            txtNombre.Size = new Size(419, 42);
+            txtNombre.Size = new Size(350, 37);
             txtNombre.TabIndex = 2;
             // 
             // txtApellidoPaterno
             // 
             txtApellidoPaterno.Font = new Font("Segoe UI", 11F);
-            txtApellidoPaterno.Location = new Point(53, 262);
-            txtApellidoPaterno.Margin = new Padding(5, 7, 5, 7);
+            txtApellidoPaterno.Location = new Point(44, 218);
+            txtApellidoPaterno.Margin = new Padding(4, 6, 4, 6);
             txtApellidoPaterno.Name = "txtApellidoPaterno";
-            txtApellidoPaterno.Size = new Size(419, 42);
+            txtApellidoPaterno.Size = new Size(350, 37);
             txtApellidoPaterno.TabIndex = 4;
             // 
             // txtApellidoMaterno
             // 
             txtApellidoMaterno.Font = new Font("Segoe UI", 11F);
-            txtApellidoMaterno.Location = new Point(53, 364);
-            txtApellidoMaterno.Margin = new Padding(5, 7, 5, 7);
+            txtApellidoMaterno.Location = new Point(44, 303);
+            txtApellidoMaterno.Margin = new Padding(4, 6, 4, 6);
             txtApellidoMaterno.Name = "txtApellidoMaterno";
-            txtApellidoMaterno.Size = new Size(419, 42);
+            txtApellidoMaterno.Size = new Size(350, 37);
             txtApellidoMaterno.TabIndex = 6;
             // 
             // txtDireccion
             // 
             txtDireccion.Font = new Font("Segoe UI", 11F);
-            txtDireccion.Location = new Point(528, 160);
-            txtDireccion.Margin = new Padding(5, 7, 5, 7);
+            txtDireccion.Location = new Point(440, 133);
+            txtDireccion.Margin = new Padding(4, 6, 4, 6);
             txtDireccion.Name = "txtDireccion";
-            txtDireccion.Size = new Size(419, 42);
+            txtDireccion.Size = new Size(350, 37);
             txtDireccion.TabIndex = 8;
             // 
             // txtTelefono
             // 
             txtTelefono.Font = new Font("Segoe UI", 11F);
-            txtTelefono.Location = new Point(528, 262);
-            txtTelefono.Margin = new Padding(5, 7, 5, 7);
+            txtTelefono.Location = new Point(440, 218);
+            txtTelefono.Margin = new Padding(4, 6, 4, 6);
             txtTelefono.Name = "txtTelefono";
-            txtTelefono.Size = new Size(419, 42);
+            txtTelefono.Size = new Size(350, 37);
             txtTelefono.TabIndex = 10;
             // 
             // txtPassword
             // 
             txtPassword.Font = new Font("Segoe UI", 11F);
-            txtPassword.Location = new Point(528, 364);
-            txtPassword.Margin = new Padding(5, 7, 5, 7);
+            txtPassword.Location = new Point(440, 303);
+            txtPassword.Margin = new Padding(4, 6, 4, 6);
             txtPassword.Name = "txtPassword";
-            txtPassword.Size = new Size(419, 42);
+            txtPassword.Size = new Size(350, 37);
             txtPassword.TabIndex = 12;
             txtPassword.UseSystemPasswordChar = true;
             // 
@@ -192,10 +193,10 @@
             BtnAgregarUsuario.FlatStyle = FlatStyle.Flat;
             BtnAgregarUsuario.Font = new Font("Segoe UI", 11F, FontStyle.Bold);
             BtnAgregarUsuario.ForeColor = Color.White;
-            BtnAgregarUsuario.Location = new Point(52, 564);
-            BtnAgregarUsuario.Margin = new Padding(4, 5, 4, 5);
+            BtnAgregarUsuario.Location = new Point(43, 470);
+            BtnAgregarUsuario.Margin = new Padding(3, 4, 3, 4);
             BtnAgregarUsuario.Name = "BtnAgregarUsuario";
-            BtnAgregarUsuario.Size = new Size(895, 54);
+            BtnAgregarUsuario.Size = new Size(746, 45);
             BtnAgregarUsuario.TabIndex = 13;
             BtnAgregarUsuario.Text = "Guardar Usuario";
             BtnAgregarUsuario.UseVisualStyleBackColor = false;
@@ -204,17 +205,31 @@
             // cmbRol
             // 
             cmbRol.FormattingEnabled = true;
-            cmbRol.Location = new Point(53, 456);
+            cmbRol.Location = new Point(44, 380);
+            cmbRol.Margin = new Padding(2, 2, 2, 2);
             cmbRol.Name = "cmbRol";
-            cmbRol.Size = new Size(419, 38);
+            cmbRol.Size = new Size(350, 33);
             cmbRol.TabIndex = 14;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold);
+            label1.ForeColor = Color.FromArgb(127, 140, 141);
+            label1.Location = new Point(44, 350);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(159, 28);
+            label1.TabIndex = 15;
+            label1.Text = "Seleccionar Rol :";
             // 
             // FrmAgregarUsuario
             // 
-            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
-            ClientSize = new Size(1019, 669);
+            ClientSize = new Size(849, 558);
+            Controls.Add(label1);
             Controls.Add(cmbRol);
             Controls.Add(BtnAgregarUsuario);
             Controls.Add(txtPassword);
@@ -231,7 +246,7 @@
             Controls.Add(lblNombre);
             Controls.Add(lblTitulo);
             FormBorderStyle = FormBorderStyle.None;
-            Margin = new Padding(5, 7, 5, 7);
+            Margin = new Padding(4, 6, 4, 6);
             Name = "FrmAgregarUsuario";
             StartPosition = FormStartPosition.CenterParent;
             Text = "Agregar Usuario";
@@ -258,5 +273,6 @@
         private System.Windows.Forms.TextBox txtPassword;
         private System.Windows.Forms.Button BtnAgregarUsuario;
         private ComboBox cmbRol;
+        private Label label1;
     }
 }
